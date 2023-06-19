54dd278991a7367df8696644cbcf17bd304f7e7c
Saad Khan <h.saadkhan501@gmail.com>
2023-06-19 17:56:10 +0500

    new routes added

--- routes/api/todo.js
+++ routes/api/todo.js
@@ -2,11 +2,17 @@ var express = require("express");
 var router = express.Router();
 
 const controllers = require("../../controllers/todo");
+const middleware = require("../../middleware/authentication");
 
-router.post("/todo", controllers.createTodo);
+router.post("/todo", middleware.authenticateMiddleware, controllers.createTodo);
 router.get("/todo", controllers.getTodo);
 router.patch("/todo/:id", controllers.updateTodo);
 router.delete("/todo/:id", controllers.deleteTodo);
 router.get("/todo/:id", controllers.getTodoById);
+router.get(
+  "/user-todo/",
+  middleware.authenticateMiddleware,
+  controllers.getTodoById
+);
 
 module.exports = router;
